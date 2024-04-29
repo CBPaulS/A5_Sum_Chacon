926024a9b188f957975af4ebffbf014ff6c6186a
CBPaulS <paulchacon13@gmail.com>
2024-04-28 19:48:07 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'A5_Sum_ChaconE2',
+  webDir: 'www'
+};
+
+export default config;
