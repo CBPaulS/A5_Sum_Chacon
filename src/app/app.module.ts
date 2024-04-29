1aab9ec24b067879a71719031a78a5960b3cec3d
CBPaulS <paulchacon13@gmail.com>
2024-04-28 22:17:30 -0500

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase} from '@angular/fire/database';
+import { environment } from '../environments/environment'; // Importa el entorno
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideDatabase(() => getDatabase()),IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Database, object, ref } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @Input() Temp: number = 20; // Porcentaje de llenado del termómetro
+  public barColor: string = ''; // Color de la barra
 
-  constructor() {}
+  constructor(private database: Database) {}
 
+  ngOnInit() {
+    // Read "Temp" value from Firebase Realtime Database
+    const tempRef = ref(this.database, '/Temp'); // Reference to the "Temp" variable
+    object(tempRef).subscribe((data) => {
+      this.Temp = data.snapshot.val(); // Set the Temp property with the value
+      this.setBarColor(); // Update the bar color based on the new Temp value
+    });
+  }
+
+  setBarColor() {
+    if (this.Temp <= 20) {
+      this.barColor = 'blue'; // Azul para valores bajos
+    } else if (this.Temp <= 50) {
+      this.barColor = 'green'; // Verde para valores medianos
+    } else {
+      this.barColor = 'red'; // Rojo para valores altos
+    }
+  }
 }
