d2ef283e467cb0c83b58c81be3570106d8924076
CBPaulS <paulchacon13@gmail.com>
2024-04-29 20:25:26 -0500

    vFin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Database, object, ref } from '@angular/fire/database';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,16 @@ export class HomePage implements OnInit {
 
   constructor(private database: Database) {}
 
-  ngOnInit() {
-    // Read "Temp" value from Firebase Realtime Database
-    const tempRef = ref(this.database, '/Temp'); // Reference to the "Temp" variable
+  async ngOnInit() {
+    // Solicitar permisos de notificaciones si no se han otorgado previamente
+    await LocalNotifications.requestPermissions();
+
+    // Leer el valor de "Temp" de la base de datos de Firebase en tiempo real
+    const tempRef = ref(this.database, '/Temp'); // Referencia a la variable "Temp"
     object(tempRef).subscribe((data) => {
-      this.Temp = data.snapshot.val(); // Set the Temp property with the value
-      this.setBarColor(); // Update the bar color based on the new Temp value
+      this.Temp = data.snapshot.val(); // Establecer la propiedad Temp con el valor
+      this.setBarColor(); // Actualizar el color de la barra según el nuevo valor de Temp
+      this.handleNotifications(this.Temp); // Enviar notificaciones según el valor de Temp
     });
   }
 
@@ -28,6 +33,43 @@ export class HomePage implements OnInit {
       this.barColor = 'green'; // Verde para valores medianos
     } else {
       this.barColor = 'red'; // Rojo para valores altos
+    }
+  }
+
+  async handleNotifications(tempValue: number) {
+    if (tempValue <= 20) {
+      // Enviar notificación para clima frío
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            title: '¡Frío!',
+            body: 'La temperatura está baja, abrígate.',
+            id: 1,
+          },
+        ],
+      });
+    } else if (tempValue <= 50) {
+      // Enviar notificación para clima neutral
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            title: 'Clima neutral',
+            body: 'La temperatura está agradable.',
+            id: 2,
+          },
+        ],
+      });
+    } else {
+      // Enviar notificación para calor extremo
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            title: '¡Calor extremo!',
+            body: 'Toma precauciones contra el calor intenso.',
+            id: 3,
+          },
+        ],
+      });
     }
   }
 }
